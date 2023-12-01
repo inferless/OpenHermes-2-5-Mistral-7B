87436ead3124021f62f21f9764b9803487805b7c
rbgo404 <150957746+rbgo404@users.noreply.github.com>
2023-12-01 11:06:16 +0000

    refix app.py

--- app.py
+++ app.py
@@ -1,26 +1,30 @@
-import os
-from vllm import SamplingParams
-from vllm import LLM
+import json
+import numpy as np
+import torch
+from transformers import AutoTokenizer
+from auto_gptq import AutoGPTQForCausalLM
+import base64
+from io import BytesIO
 
 
 class InferlessPythonModel:
-    def initialize(self):
-        self.llm = LLM(
-          model="teknium/OpenHermes-2.5-Mistral-7B",
-          quantization="awq",
-          dtype="float16")
+  def initialize(self):
+      self.tokenizer = AutoTokenizer.from_pretrained("TheBloke/OpenHermes-2.5-Mistral-7B-GPTQ", use_fast=True)
+      self.model = AutoGPTQForCausalLM.from_quantized(
+        "TheBloke/OpenHermes-2.5-Mistral-7B-GPTQ",
+        use_safetensors=True,
+        device="cuda:0",
+        quantize_config=None,
+        inject_fused_attention=False
+      )
 
-    def infer(self, inputs):
-        prompts = inputs["questions"]
-        sampling_params = SamplingParams(
-            temperature=0.75,
-            top_p=1,
-            max_tokens=200
-        )
-        result = self.llm.generate(prompts, sampling_params)
-        result_output = [output.outputs[0].text for output in result]
+  def infer(self, inputs):
+    prompt = inputs["prompt"]
+    input_ids = self.tokenizer(prompt, return_tensors='pt').input_ids.cuda()
+    output = self.model.generate(inputs=input_ids, temperature=0.7, max_new_tokens=200)
+    result = self.tokenizer.decode(output[0])
+    return {"generated_result": result}
 
-        return {"result": result_output[0]}
-
-    def finalize(self, args):
-        pass
+  def finalize(self,args):
+    self.tokenizer = None
+    self.model = None
