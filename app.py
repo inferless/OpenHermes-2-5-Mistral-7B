d52d7f6f16987a7e35272fe87dbe1c610ca8f879
rbgo404 <150957746+rbgo404@users.noreply.github.com>
2023-12-01 11:01:00 +0000

    fix for app

--- app.py
+++ app.py
@@ -5,14 +5,17 @@ from vllm import LLM
 
 class InferlessPythonModel:
     def initialize(self):
-        self.llm = LLM("teknium/OpenHermes-2.5-Mistral-7B")
+        self.llm = LLM(
+          model="teknium/OpenHermes-2.5-Mistral-7B",
+          quantization="awq",
+          dtype="float16")
+
     def infer(self, inputs):
         prompts = inputs["questions"]
         sampling_params = SamplingParams(
             temperature=0.75,
             top_p=1,
-            max_tokens=200,
-            presence_penalty=1.15,
+            max_tokens=200
         )
         result = self.llm.generate(prompts, sampling_params)
         result_output = [output.outputs[0].text for output in result]
